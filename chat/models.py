dfc7a8870cf34e1616e21defbbba619da5eba79c
lucorus <nikita.hom1977@gmail.com>
2024-06-28 21:42:11 +0400

    add receivers

--- chat/models.py
+++ chat/models.py
@@ -1,12 +1,14 @@
 from django.contrib.auth import get_user_model
 from django.db import models
+from django.db.models.signals import pre_save, post_delete
+from django.dispatch import receiver
 from slugify import slugify
 
 
 class Group(models.Model):
     title = models.CharField(max_length=64, unique=True, verbose_name='Название')
     slug = models.SlugField(unique=True)
-    image = models.ImageField(upload_to='group_image/', null=True, blank=True, verbose_name='Изображение')
+    image = models.ImageField(upload_to='images/group_image/', null=True, blank=True, verbose_name='Изображение')
     description = models.TextField(max_length=512, verbose_name='Описание')
     participants = models.ManyToManyField(get_user_model(), related_name='group_member', verbose_name='Участники')
 
@@ -39,4 +41,21 @@ class Message(models.Model):
         ordering = ['id']
         verbose_name = 'Сообщение'
         verbose_name_plural = 'Сообщения'
+
+
+@receiver(post_delete)
+def delete_related_image(instance, **kwargs):
+    """При удалении фотографии группы, фото удаляется из папки"""
+    if instance.image:
+        storage, path = instance.image.storage, instance.image.path
+        storage.delete(path)
+
+
+@receiver(pre_save, sender=Group)
+def delete_related_image_edit(sender, instance, **kwargs):
+    """При обновлении фотографии группы, её старое фото удаляется из папки"""
+    if instance.id:
+        old_instance = sender.objects.filter(pk=instance.id).first()
+        if old_instance.image != instance.image:
+            old_instance.image.delete(save=False)
 
